e13413efc44a43f2af09884bfb0314c99d7926e2
unknown <inv2004@gmail.com>
2018-10-17 20:13:54 -0400

    bad idea

--- src/structs/wsfeed.rs
+++ src/structs/wsfeed.rs
@@ -129,7 +129,7 @@ pub struct Level2UpdateRecord {
     pub size: f64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 #[serde(untagged)]
 #[serde(rename_all = "camelCase")]
 pub enum Ticker {
