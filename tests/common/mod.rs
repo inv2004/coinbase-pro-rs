711904cbac29744f78e22b908938f21af7846de7
unknown <inv2004@gmail.com>
2018-11-21 16:14:59 -0500

    comment test logger

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,9 +1,9 @@
-extern crate pretty_env_logger;
+//extern crate pretty_env_logger;
 use std::{thread, time};
 
 static DELAY_TIMEOUT: u64 = 200;
 
 pub fn delay() {
     thread::sleep(time::Duration::from_millis(DELAY_TIMEOUT));
-    pretty_env_logger::init();
+//    pretty_env_logger::init();
 }
