ef43ba77d5c45778c9bccb6d8905e9c9fba85dfe
Mike Lubinets <public@mersinvald.me>
2019-02-05 22:14:37 +0300

    thread-local runtime storage

--- src/adapters.rs
+++ src/adapters.rs
@@ -2,6 +2,8 @@ extern crate tokio;
 
 use super::error::CBError;
 use hyper::rt::Future;
+use tokio::runtime::current_thread::Runtime;
+use std::cell::RefCell;
 
 pub trait Adapter<T> {
     type Result;
@@ -12,14 +14,20 @@ pub trait Adapter<T> {
 
 pub struct Sync;
 
+thread_local! {
+    static RUNTIME: RefCell<Runtime> = RefCell::new(Runtime::new().unwrap());
+}
+
 impl<T> Adapter<T> for Sync {
     type Result = Result<T, CBError>;
     fn process<F>(f: F) -> Self::Result
     where
         F: Future<Item = T, Error = CBError> + Send + 'static,
     {
-        let mut rt = tokio::runtime::current_thread::Runtime::new().unwrap();
-        rt.block_on(f)
+        RUNTIME.with(|rt| {
+            rt.borrow_mut().block_on(f)
+        })
+
     }
 }
 
